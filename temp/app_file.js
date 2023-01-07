1736b0ddd840e1a00f2a42d1c83b757689db1bc6
seungjaejeon <wjstmdwo2000@naver.com>
2023-01-07 16:23:37 +0900

    simple project finish

--- temp/app_file.js
+++ temp/app_file.js
@@ -9,19 +9,41 @@ app.set('view engine', 'jade');
 app.listen(3000, function(){
     console.log('Connected 3000port');
     console.log(`server running at http://localhost:3000`);
-})
+});
 app.get('/topic/new', function(req, res){
-    res.render('new');
-})
-app.get('/topic', function(req, res){
     fs.readdir('data', function(err, files){
         if(err){
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        res.render('view', {topics:files});
+        res.render('new', {topics: files});
     })
-})
+});
+app.get(['/topic', '/topic/:id'], function(req, res){
+    //id값이 없을때 코드
+    fs.readdir('data', function(err, files){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        var id = req.params.id;
+        //id값이 있을때 코드
+        if(id){
+        fs.readFile('data/'+id,'utf8', function(err,data){
+            if(err){
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+            res.render('view', {topics: files, title:id, descriptions:data});
+        })
+        }
+        //id값이 없을때 코드
+        else{
+            res.render('view', {topics:files, title:'Welcome', descriptions:'Hello, Server'})
+        }
+    })
+});
+
 app.post('/topic', function(req, res){
     var title = req.body.title;;
     var description = req.body.description;
@@ -30,6 +52,6 @@ app.post('/topic', function(req, res){
             console.log('error 발생\n' + err);
             res.status(500).send('Internal Server Error')
         }
-        res.send('Success!');
+        res.redirect('/topic/'+title);
     });
-})
+});
